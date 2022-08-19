b392f22c9836a02de782f7035aa1d26ff83b8672
akyarbrough <ayarbrough0816@gmail.com>
2022-08-18 19:33:49 -0500

    Commit

---
+++ Day 2 - Foundations - Types & Variables, Operators, Comparisons, Conditionals, Flows & Loops/variables.js
@@ -0,0 +1,27 @@
+//string
+const name = "amanda";
+
+//number
+const age = 44;
+
+//boolean
+const college = true;
+
+//null
+const degree = null;
+
+//undefined
+password = 12345;
+
+//object
+let clothing = {
+    size: "large",
+    color: "red",
+    style: "womans"
+}
+
+//array
+const students = ['ashlyn', 'robert', 'jessica', 'timmy'];
+
+//no default value declaration
+let score;
