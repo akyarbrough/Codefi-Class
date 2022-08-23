7249ca1762a45f91b37a8be1c1e21d8fa9b40ca9
akyarbrough <ayarbrough0816@gmail.com>
2022-08-22 20:18:12 -0500

    Comitt

---
+++ Day 3 - Functions & Objects/basic.js
@@ -0,0 +1,5 @@
+function volume (length, width, height) {
+    return (length * width * height);
+}
+
+console.log(volume(5, 7, 9));
---
+++ Day 3 - Functions & Objects/intermediate.js
@@ -0,0 +1,19 @@
+let user = [
+    {
+        name: "Shelly",
+        age: 35,
+        occupation: "doctor"
+    },{
+        name: "Robert",
+        age: 32,
+        occupation: "lawyer"
+    },{
+        name: "Anah",
+        age: 22,
+        occupation: "cashier"
+    },{
+        name: "Frank",
+        age: 68,
+        occupation: "retired"
+    }
+]
