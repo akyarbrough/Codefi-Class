4434e066f3d4173ff70200f3a9d3fca9f3ef156c
akyarbrough <ayarbrough0816@gmail.com>
2022-08-25 20:06:29 -0500

    Commit

---
+++ Day 4/basic_exercise/exercise_basic.js
@@ -0,0 +1,8 @@
+
+  
+function changeBackAndText(){
+        document.body.style.backgroundColor = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`
+        document.body.style.color = "yellow";
+    }
+    //Step 1:  Create a button
+    //Step 2:  Change the background and text color when clicked
