6fdaba7e24ef9362ed89801a79daade8ee158aec
akyarbrough <ayarbrough0816@gmail.com>
2022-08-25 20:27:55 -0500

    Commit

---
+++ Day 4/exercise_intermediate/intermediate.js
@@ -0,0 +1,15 @@
+countTrue([true, false, false, true, false]) //   ➞    2
+
+countTrue([false, false, false, false]) //   ➞    0
+
+countTrue([])  //  ➞    0
+
+//Step 1:  Create a function called countTrue that counts the number of 'true' in an array
+
+function countTrue(){
+    let i = 0; i < 
+}
+
+
+
+//Step 2:  Return 0 if there is an empty array
